54419936d70d20de71d6daa9527cd42e608259d3
Marc Hensel <121319337+MarcOnTheMoon@users.noreply.github.com>
2024-10-07 20:09:52 +0200

    Added c++ and python dirs

--- src/c++/lecture/02_Images/Sandbox/Sandbox.cpp
+++ src/c++/lecture/02_Images/Sandbox/Sandbox.cpp
@@ -4,21 +4,14 @@
  * Author: Marc Hensel, http://www.haw-hamburg.de/marc-hensel
  * Project: https://github.com/MarcOnTheMoon/imaging_learners/
  * Copyright: 2023, Marc Hensel
- * Version: 2023.09.09
+ * Version: 2024.10.07
  * License: CC BY-NC-SA 4.0, see https://creativecommons.org/licenses/by-nc-sa/4.0/deed.en
  *****************************************************************************************************/
-
-/* Compiler settings */
-#define _CRT_SECURE_NO_WARNINGS		// Enable getenv()
 
 /* Include files */
 #include <iostream>
 #include <string>
 #include <opencv2/opencv.hpp>
-
-/* Defines */
-#define DATA_ROOT_PATH getenv("ImagingData")  // Read environment variable
-#define INPUT_FILE "/images/misc/Docks.jpg"
 
 /* Namespaces */
 using namespace cv;
@@ -26,11 +19,7 @@ using namespace std;
 
 /* Main function */
 int main(void) {
-	cv::Mat image = cv::imread("D:/Docks.jpg", cv::IMREAD_GRAYSCALE);
 
-	cv::imshow("Image", image);
-	cv::imwrite("D:/Gray.jpg", image);
-
-	cv::waitKey(0);
+	waitKey(0);
 	return 0;
 }
